9a2d6c6f1ee84ae9202b435d157d30d93ffc5b6d
ChrisRodriguez <chrisreidr@gmail.com>
2022-09-06 09:19:31 -0400

    added nav bar

--- controllers/pumping.js
+++ controllers/pumping.js
@@ -27,7 +27,7 @@ module.exports = {
             console.log('Deleted Pumping')
             res.redirect("/pumping");
         }catch(err){
-            console.log(err, 'pumping')
+            console.log(err)
             res.redirect("/pumping");
         }
     }
