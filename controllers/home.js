ff9821cd0df45c6eb9540a91dafaf228df00d4c1
ChrisRodriguez <chrisreidr@gmail.com>
2022-09-19 08:05:00 -0400

    home controller pather for newToPumping route was not properly set. Fixed it and added content to page for a tes, it works

--- controllers/home.js
+++ controllers/home.js
@@ -1,10 +1,7 @@
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
-    }
-}
-
-module.exports = {
+    },
     getNewToPumping: (req,res)=>{
         res.render('newToPumping.ejs')
     }
